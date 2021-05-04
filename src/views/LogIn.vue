9581676ab9a3cd469b5e201626dd49215e3e4ce2
joakimliden <joakim.liden@iths.se>
2021-05-04 14:15:10 +0200

    new user-dropdown

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="login">
     <header>
-      <button v-if="userState === 'default'" class="btn btn-primary-user" @click="changeUserState('edit')">Users</button>
+      <button v-if="userState === 'default'" class="btn btn-primary-user" @click="changeUserState('edit')">Users
+      </button>
       <button v-else class="btn btn-red-user" @click="changeUserState('default')">Back</button>
 
-      <div class="center">
-        <vue-dropdown :config="config"></vue-dropdown>
+      <div id='container' style="margin:50px auto 0; width:250px;">
+        <ejs-dropdownlist id='dropdownlist' :dataSource='usersData' placeholder='Select a game'></ejs-dropdownlist>
       </div>
 
       <div v-if="userState === 'edit'">
-        <form method="POST" id="formUser" class="new-user" @submit.prevent="postData('http://127.0.0.1:3000/api/users')">
+        <form method="POST" id="formUser" class="new-user"
+              @submit.prevent="postData('http://127.0.0.1:3000/api/users')">
           <div>
             <input type="text" id="userName" name="userName" v-model="formData.userName">
             <input
@@ -25,78 +27,67 @@
     <ul>
       <li v-for="user in user" v-bind:key="user.userId">{{ user.highScore }} {{ user.userName }}</li>
     </ul>
+
   </div>
+
+
 </template>
 
 <script>
 
-import VueDropdown from 'vue-dynamic-dropdown'
+import Vue from 'vue';
+import {DropDownListPlugin} from "@syncfusion/ej2-vue-dropdowns";
 
-  export default {
+Vue.use(DropDownListPlugin);
+
+export default Vue.extend({
   name: "LogIn",
-  data() {
-  return {
-    config: {
-      options: [
-        {
-          value: "TEST"
-        },
-        {
-          value: "option 2"
-        },
-        {
-          value: "option 3"
-        },
-      ],
-      width: 300,
-      prefix: "The",
-      backgroundColor: "green"
-    },
-  userState: 'default',
-  newUserName: '',
-  user: [],
-  formData: {
-  userName: ''
-}
-}
-},
-  mounted(){
-  fetch('http://127.0.0.1:3000/api/users/')
-  .then((response) => {
-  return response.json();
-})
-  .then((data) => {
-  console.log(data.user);
-  this.user = data.user;
-});
-},
+  data: function () {
+    return {
+      usersData: ['Marcus', 'Joakim', 'Hans'],
+      userState: 'default',
+      newUserName: '',
+      user: [],
+      formData: {
+        userName: ''
+      }
+    }
+  },
+  mounted() {
+    fetch('http://127.0.0.1:3000/api/users/')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data.user);
+          this.user = data.user;
+        });
+  },
   methods: {
-  changeUserState: function (newUserState) {
-  this.userState = newUserState
-},
+    changeUserState: function (newUserState) {
+      this.userState = newUserState
+    },
 
-  async postData(url='') {
-  await fetch(url, {
-  method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  mode: 'cors', // no-cors, *cors, same-origin
-  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  credentials: 'same-origin', // include, *same-origin, omit
-  headers: {
-  'Content-Type': 'application/json'
-},
-  redirect: 'follow', // manual, *follow, error
-  referrerPolicy: 'no-referrer', // no-referrer, *client
-  body: JSON.stringify({userName: this.formData.userName, highScore: 0}) // body data type must match "Content-Type" header
+    async postData(url = '') {
+      await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify({userName: this.formData.userName, highScore: 0}) // body data type must match "Content-Type" header
+      });
+
+      //return response.json();
+      location.reload();
+    }
+  },
+  components: {}
 });
-
-  //return response.json();
-  location.reload();
-}
-},
-  components: {
-    VueDropdown
-}
-}
 </script>
 
 <style scoped>
@@ -141,4 +132,10 @@ li {
   justify-content: center;
   align-items: center;
 }
+</style>
+
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
 </style>
