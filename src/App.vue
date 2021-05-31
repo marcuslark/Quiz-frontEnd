1f80c50b70926d6015b9f2282e2cf6f2b73590f2
Hans Sandblom <hans.sandblom@iths.se>
2021-05-31 12:11:47 +0200

    Added collapsing menu when on mobile, added vuetify navigation drawer on mobile

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <Navbar />
 
     <v-main>
-      <router-view/>
+      <router-view></router-view>
     </v-main>
 
   </v-app>
@@ -15,30 +15,11 @@
 
 import Navbar from '@/components/Navbar'
 
-
-// <!--import {firebase} from "@firebase/app";-->
-// <!--import 'firebase/auth';-->
-// <!--import 'firebase/firestore'-->
-//
 export default {
   name: 'App',
-  components: {Navbar}
-}
-//
-// <!--  methods: {-->
-// <!--    logout() {-->
-// <!--      this.$store.dispatch('logout')-->
-// <!--    },-->
-//
-// <!--    checkIfLoggedIn() {-->
-// <!--      const auth = firebase.auth().currentUser-->
-//
-// <!--      if (auth) {-->
-// <!--        return true-->
-// <!--      } else {-->
-// <!--        return false-->
-// <!--      }-->
-// <!--    },-->
-// <!--  }-->
-// <!--}-->
+  components: {Navbar},
+  data: () => ({
+    drawer: true,
+  }),
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,35 +3,72 @@
 
   <nav>
     <v-toolbar color="indigo">
+      <v-app-bar-nav-icon v-if="mobile" class="orange--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase black&#45;&#45;text">
         <span class="font-weight-bold">City </span>
         <span>Quiz</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-
-        <v-btn color="orange" to="/">Home</v-btn>
-        <v-btn color="orange" v-if="!checkIfLoggedIn()" to="/login">Log In</v-btn>
-        <v-btn color="orange" v-if="checkIfLoggedIn()" to="/logout">Log Out</v-btn>
-        <v-btn color="orange" to="/quiz">Quiz</v-btn>
-        <v-btn color="orange" v-if="checkIfLoggedIn()" to="/myprofile">My Profile</v-btn>
-        <v-btn color="orange" to="/highscore">High scores</v-btn>
-
+        <v-btn v-if="!mobile" color="orange" to="/">Home</v-btn>
+        <v-btn color="orange" v-if="!checkIfLoggedIn() && !mobile" to="/login">Log In</v-btn>
+        <v-btn color="orange" v-if="checkIfLoggedIn() && !mobile" to="/logout">Log Out</v-btn>
+        <v-btn v-if="!mobile" color="orange" to="/quiz">Quiz</v-btn>
+        <v-btn color="orange" v-if="checkIfLoggedIn() && !mobile" to="/myprofile">My Profile</v-btn>
+        <v-btn v-if="!mobile" color="orange" to="/highscore">High scores</v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-if="mobile" v-model="drawer" app class="secondary">
+      <v-list v-if="!checkIfLoggedIn()">
+        <v-list-item v-for="link in linksLogin" :key="link.text" router :to="link.route">
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list v-if="checkIfLoggedIn()">
+        <v-list-item v-for="link in linksLogout" :key="link.text" router :to="link.route">
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
   </nav>
-
-
 
 </template>
 
-
 <script>
 
-import {firebase} from "@firebase/app";
-import 'firebase/auth';
-import 'firebase/firestore';
+import {firebase} from "@firebase/app"
+import 'firebase/auth'
+import 'firebase/firestore'
 
  export default {
-   name: 'Navbar',
+   data: () => ({
+     name: 'Navbar',
+     drawer: false,
+     linksLogin: [
+       { text: 'Home', route: '/'},
+       { text: 'Log In', route: '/login'},
+       { text: 'Quiz', route: '/quiz'},
+       { text: 'High scores', route: '/highscore'},
+     ],
+     linksLogout: [
+       { text: 'Home', route: '/'},
+       { text: 'Log Out', route: '/logout'},
+       { text: 'Quiz', route: '/quiz'},
+       { text: 'My Profile', route: '/myprofile'},
+       { text: 'High scores', route: '/highscore'},
+     ]
+
+   }),
+
+   computed: {
+     mobile() {
+       return this.$vuetify.breakpoint.mobile
+     },
+   },
 
   methods: {
     logout() {
