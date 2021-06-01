8b5fcd3e49e154da54f4afe7f6a822ababc8dd57
marcuslark <marcus.lark.stahlberg@iths.se>
2021-06-01 11:40:10 +0200

    removed trash code from Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,14 +15,7 @@
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      userState: 'default',
-      newUserName: '',
-      topThree: [2]
-    }
-  },
-
+  
   methods: {
     redirect() {
       this.$router.push('Quiz')
@@ -33,54 +26,10 @@ export default {
 
 
 <style scoped>
-.btn-start {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: white; /* Green background */
-  border: 1px solid green; /* Green border */
-  color: white; /* White text */
-  padding: 10px 24px; /* Some padding */
-  cursor: pointer; /* Pointer/hand icon */
-}
 .home-img {
   max-width: 60%;
   display: block;
   margin-left: auto;
   margin-right: auto;
-}
-
-li {
-  list-style: none;
-}
-
-.btn-primary-user {
-  position: absolute;
-  top: 0;
-  right: 0;
-  background: #6CB2EB;
-}
-
-.btn-primary-user:hover {
-  background: #3490DC;
-  color: #fff;
-}
-
-.btn-red-user {
-  position: absolute;
-  top: 0;
-  right: 0;
-  background: #EF5753;
-}
-
-.btn-red-user:hover {
-  background: #E3342F;
-  color: #fff;
-}
-
-.new-user {
-  position: absolute;
-  top: 0;
-  right: 60px;
 }
 </style>
